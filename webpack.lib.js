55e8101f1138002667f1ca511adf5f1816937fea
Justin Walsh <contact.me@thejustinwalsh.com>
2021-12-06 07:09:58 -0700

    fix: library bundle external react

--- webpack.lib.js
+++ webpack.lib.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './src/index.ts',
+  target: 'node',
+  externals: [nodeExternals()],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
-    libraryTarget: "umd",
+    libraryTarget: "commonjs2",
     library: "react-defold"
   },
   resolve: {
