a06ae4b33be38c300062bf090690aad2b4fbffc0
Justin Walsh <contact.me@thejustinwalsh.com>
2021-12-08 06:15:48 -0700

    fix: esm module output only

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import pkg from "./package.json";
+import typescript from 'rollup-plugin-typescript2';
+
+// Excluded dependencies
+const EXTERNAL = Object.keys(pkg.devDependencies);
+
+export default {
+  input: ["src/index.ts"],
+  output: {
+    dir: "dist",
+    sourcemap: true,
+    format: "esm",
+    exports: "named",
+    preserveModules: true,
+    preserveModulesRoot: "src"
+  },
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    typescript()
+  ],
+  external: EXTERNAL
+};
